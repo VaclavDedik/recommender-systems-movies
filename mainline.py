c5db07efd3711351d4d56c778566811600222dc4
Vaclav Dedik <vaclav.dedik@gmail.com>
2014-10-26 21:11:43 +0100

    Change maxiter

--- mainline.py
+++ mainline.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     train = load_data("ratings-train.csv")
     test = load_data("ratings-test.csv")
     print "Training the baseline predictor on %s data points..." % len(train)
-    m = models.BaseModel(maxiter=110)
+    m = models.BaseModel(maxiter=120)
     m.train(train)
     print "The test set contains %s data points." % len(test)
     print "RMSE on the test set is %.4f" % m.test(test)
