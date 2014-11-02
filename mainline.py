495154eca43cf00d317d15358fb5a189c8ac6c38
Vaclav Dedik <vaclav.dedik@gmail.com>
2014-11-02 14:19:05 +0100

    Learning curves

--- mainline.py
+++ mainline.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
 from movies import models
-from movies.utils import load_data
+from movies.utils import load_data, get_learning_curves, plot_learning_curves
 
 if __name__ == "__main__":
     train = load_data("ratings-train.csv")
     test = load_data("ratings-test.csv")
     print "Training the baseline predictor on %s data points..." % len(train)
     m = models.BaseModel(maxiter=100)
-    m.train(train)
-    print "The test set contains %s data points." % len(test)
-    print "RMSE on the test set is %.4f" % m.test(test)
+    #m.train(train)
+    #print "The test set contains %s data points." % len(test)
+    #print "RMSE on the test set is %.4f" % m.test(test)
+
+    # Learning curves
+    learning_curves = get_learning_curves(m, train, test)
+    print "Learning curves: \n%s" % str(learning_curves)
+    plot_learning_curves(learning_curves)
--- movies/utils.py
+++ movies/utils.py
@@ -1,3 +1,7 @@
+import random
+import matplotlib.pyplot as plt
+
+
 def load_data(filename):
     """
     Returns a list of data points.
@@ -12,4 +16,48 @@ def load_data(filename):
 
 def rmse(a, b):
     """ Returns RMSE between two lists. """
+    if not a or not b:
+        return 0
     return (sum([(i - j) ** 2 for i, j in zip(a, b)]) / float(len(a))) ** 0.5
+
+
+def get_learning_curves(model, train, test):
+    random.shuffle(train)
+    learning_curves = []
+    start = 0
+    # Learning curves on 0..train_set/10
+    while 2 ** start < len(train)/10:
+        curr_range = 2 ** start
+        curr_train = train[:curr_range]
+        print "Training on %s data..." % curr_range
+        model.train(curr_train)
+        lc_train = model.test(curr_train)
+        lc_test = model.test(test)
+        percentage = len(curr_train)/float(len(train)) * 100
+        learning_curves.append((lc_train, lc_test, percentage))
+        start += 1
+
+    # Learning curves on train_set/10..train_set
+    for i in range(1, 11):
+        curr_range = len(train) * i / 10
+        curr_train = train[:curr_range]
+        print "Training on %s data..." % curr_range
+        model.train(curr_train)
+        lc_train = model.test(curr_train)
+        lc_test = model.test(test)
+        percentage = len(curr_train)/float(len(train)) * 100
+        learning_curves.append((lc_train, lc_test, percentage))
+
+    return learning_curves
+
+
+def plot_learning_curves(learning_curves):
+    print "Plotting learning curves..."
+    plt.plot([y for _, _, y in learning_curves],
+             [x for x, _, _ in learning_curves], label="Train set")
+    plt.plot([y for _, _, y in learning_curves],
+             [x for _, x, _ in learning_curves], label="Test set")
+    plt.ylabel("RMSE")
+    plt.xlabel("% of training data")
+    plt.legend()
+    plt.show()
